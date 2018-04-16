2631deb25599144812f7e6b367ab697b8fdf9e51
wangjie <728199993@qq.com>
2018-04-16 18:17:01 +0800

    complete the rest function of FizzBuzzGame.java

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -16,13 +16,28 @@ public class FizzBuzzGame {
     }
 
     public void start(int count) {
-
+        if(teacher.isReasonableNum()){
+            for (int i = 1; i <= count; i++) {
+                Student student = new Student(i);
+                results.add(student.sayContent(teacher.getNumber1()));
+            }
+            outputAllcontent();
+        }else{
+            System.out.println("请重新想三个10以内的不重复数字！");
+        }
     }
 
     public void init(AquireReader reader) {
         start(reader.read());
 
     }
+
+    public void outputAllcontent(){
+        for (String item : results) {
+            System.out.println(item);
+        }
+    }
+
 
     public List<String> getResults() {
         return results;
