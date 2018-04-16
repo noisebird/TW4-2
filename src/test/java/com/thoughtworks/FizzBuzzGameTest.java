8ef0f43f78f3e8e8d2fdc2f8d8a798f647aacf98
wangjie <728199993@qq.com>
2018-04-16 18:18:43 +0800

    add rest test method in FizzBuzzGameTest.java

--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -43,4 +43,83 @@ public class FizzBuzzGameTest {
         assertEquals("1",game.getResults().get(0));
     }
 
+    @Test
+    public void should_person_3_then_return_fizz() throws Exception {
+        when(reader.read()).thenReturn(100);
+        game.init(reader);
+        assertEquals("Fizz",game.getResults().get(2));
+
+    }
+    @Test
+    public void should_person_5_then_return_fizz() throws Exception {
+        when(reader.read()).thenReturn(100);
+        game.init(reader);
+        assertEquals("Buzz",game.getResults().get(4));
+
+    }
+    @Test
+    public void should_person_7_then_return_fizz() throws Exception {
+        when(reader.read()).thenReturn(100);
+        game.init(reader);
+        assertEquals("Whizz",game.getResults().get(6));
+
+    }
+    @Test
+    public void should_person_13_then_return_fizz() throws Exception {
+        when(reader.read()).thenReturn(100);
+        game.init(reader);
+        assertEquals("Fizz",game.getResults().get(12));
+
+    }
+    @Test
+    public void should_person_63_then_return_fizz() throws Exception {
+        when(reader.read()).thenReturn(100);
+        game.init(reader);
+        assertEquals("Fizz",game.getResults().get(62));
+
+    }
+    @Test
+    public void should_person_15_then_return_fizz() throws Exception {
+        when(reader.read()).thenReturn(100);
+        game.init(reader);
+        assertEquals("FizzBuzz",game.getResults().get(14));
+
+    }
+    @Test
+    public void should_person_21_then_return_fizz() throws Exception {
+        when(reader.read()).thenReturn(100);
+        game.init(reader);
+        assertEquals("FizzWhizz",game.getResults().get(20));
+
+    }
+    @Test
+    public void should_person_35_then_return_fizz() throws Exception {
+        when(reader.read()).thenReturn(100);
+        game.init(reader);
+        assertEquals("Fizz",game.getResults().get(34));
+
+    }
+    @Test
+    public void should_person_105_then_return_fizz() throws Exception {
+        when(reader.read()).thenReturn(110);
+        game.init(reader);
+        assertEquals("FizzBuzzWhizz",game.getResults().get(104));
+
+    }
+
+    @Test
+    public void other_test() throws Exception {
+//  verify验证行为是否发生
+        List<Integer> mock=mock(List.class);
+        mock.add(1);
+        mock.add(2);
+        mock.clear();
+        verify(mock).add(1);
+//        verify(mock).add(3);
+        verify(mock).clear();
+//  模拟数据
+        Iterator<String> it=mock(Iterator.class);
+        when(it.next()).thenReturn("hello ").thenReturn("world").thenReturn(" !");
+        assertEquals("hello world !",it.next()+it.next()+it.next());
+    }
 }
