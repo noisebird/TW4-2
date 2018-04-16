a54bf268afad5289218176643913c0e2963f1e4e
wangjie <728199993@qq.com>
2018-04-16 18:12:11 +0800

    complete the class Student.java

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -6,14 +6,23 @@ import java.util.List;
 
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<String>();
+    private Teacher teacher=new Teacher(3,5,7);
+
+    public FizzBuzzGame() {
+    }
+
+    public FizzBuzzGame(Teacher teacher) {
+        this.teacher=teacher;
+    }
 
     public void start(int count) {
+
     }
 
     public void init(AquireReader reader) {
+        start(reader.read());
 
     }
-
 
     public List<String> getResults() {
         return results;
--- src/main/java/com/thoughtworks/Student.java
+++ src/main/java/com/thoughtworks/Student.java
@@ -4,4 +4,42 @@ package com.thoughtworks;
  * Created by wangjie on 2018/4/10.
  */
 public class Student {
+    private int id;
+
+    public Student() {
+    }
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String sayContent(int firstNum) {
+
+        String content = "";
+        if ((id+"").indexOf(firstNum + "") != -1) {
+            return "Fizz";
+        }
+        if (id % 3 == 0) {
+            content += "Fizz";
+        }
+        if (id % 5 == 0) {
+            content += "Buzz";
+        }
+        if (id % 7 == 0) {
+            content += "Whizz";
+        }
+        if (content.equals("")) {
+            content=id+"";
+        }
+        return content;
+
+    }
 }
